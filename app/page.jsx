01be07a6e15d31415f9e6726e1458f1c14cb2537
Alvaro Capaceta <ingcapadev@gmail.com>
2023-11-14 20:29:09 -0700

    feat(): React server components

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,13 @@
+import Users from '../components/Users';
+
 const page = () => {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Next.js</p>
+
+      <Users />
+    </>
+  );
 };
 export default page;
---
+++ components/Users.jsx
@@ -0,0 +1,12 @@
+'use client';
+const Users = () => {
+  return (
+    <button
+      onClick={() => {
+        alert('Hello World');
+      }}>
+      Hello World
+    </button>
+  );
+};
+export default Users;
